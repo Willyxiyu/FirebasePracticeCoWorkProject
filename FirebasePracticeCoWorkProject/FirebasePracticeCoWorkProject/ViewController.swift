6a4f3c34fb7648362998ee839823fee58e06dfc9
Willy <willyxiyu@gmail.com>
2021-09-22 13:42:23 +0800

    creat project

---
+++ FirebasePracticeCoWorkProject/FirebasePracticeCoWorkProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebasePracticeCoWorkProject
+//
+//  Created by 林希語 on 2021/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
